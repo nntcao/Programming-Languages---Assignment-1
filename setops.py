051cd346cd13fffe2b23c7ddb0e0110b1d859fd0
nntcao <81784608+nntcao@users.noreply.github.com>
2024-02-06 15:22:56 -0600

    initial commit

---
+++ setops.py
@@ -0,0 +1,45 @@
+
+def delimiter_helper_get_word(string, symbol):
+    if not string or string[0] == symbol:
+        return ""
+    return string[0] + delimiter_helper_get_word(string[1:], symbol)
+
+def delimiter_helper_next(string, symbol):
+    if not string:
+        return []
+    if string[0] == symbol and string[1] != symbol:
+        return [delimiter_helper_get_word(string[1:], symbol)] + delimiter_helper_next(string[1:], symbol)
+    return delimiter_helper_next(string[1:], symbol)
+
+def delimiter(string, symbol):
+    if not string:
+        return []
+    if string[0] != symbol:
+        return [delimiter_helper_get_word(string[0:], symbol)] + delimiter_helper_next(string[1:], symbol)
+    return delimiter_helper_next(string[1:], symbol)
+
+def remove_symbol(string, symbol):
+    if not string:
+        return ""
+    if string[0] == symbol:
+        return remove_symbol(string[1:], symbol)
+    return string[0] + remove_symbol(string[1:], symbol)
+
+def to_lower(string):
+    if not string:
+        return ""
+    if 65 <= ord(string[0]) and ord(string[0]) <= 90:
+        return chr(ord(string[0]) + 32) + to_lower(string[1:])
+    return string[0] + to_lower(string[1:])
+
+def main():
+    print(delimiter("sudden attack", " "))
+    print(delimiter("      sudden attack", " "))
+    print(delimiter(" sudden          attack", " "))
+    print(delimiter(" sudden          attack parrot chopper test", " "))
+    print(remove_symbol("Dog jump over moon. And shot, at sun. And become world.", "."))
+    print(to_lower("aaAAAAaa!.AAaaAAA"))
+
+
+if __name__ == "__main__":
+    main()
