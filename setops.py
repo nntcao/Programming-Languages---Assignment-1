d6661989101c231b8698688bff5262d5bece4605
nntcao <81784608+nntcao@users.noreply.github.com>
2024-02-06 22:36:55 -0600

    Add set operators

--- setops.py
+++ setops.py
@@ -4,12 +4,14 @@ def delimiter_helper_get_word(string, symbol):
         return ""
     return string[0] + delimiter_helper_get_word(string[1:], symbol)
 
+
 def delimiter_helper_next(string, symbol):
     if not string:
         return []
     if string[0] == symbol and string[1] != symbol:
         return [delimiter_helper_get_word(string[1:], symbol)] + delimiter_helper_next(string[1:], symbol)
     return delimiter_helper_next(string[1:], symbol)
+
 
 def delimiter(string, symbol):
     if not string:
@@ -18,6 +20,7 @@ def delimiter(string, symbol):
         return [delimiter_helper_get_word(string[0:], symbol)] + delimiter_helper_next(string[1:], symbol)
     return delimiter_helper_next(string[1:], symbol)
 
+
 def remove_symbol(string, symbol):
     if not string:
         return ""
@@ -25,10 +28,12 @@ def remove_symbol(string, symbol):
         return remove_symbol(string[1:], symbol)
     return string[0] + remove_symbol(string[1:], symbol)
 
+
 def remove_symbols(string, symbols):
     if not symbols:
         return string
     return remove_symbols(remove_symbol(string, symbols[0]), symbols[1:])
+
 
 def to_lower(string):
     if not string:
@@ -38,6 +43,8 @@ def to_lower(string):
     return string[0] + to_lower(string[1:])
 
 # checks if element exists in list l
+
+
 def exists(l, element):
     if not l:
         return False
@@ -45,8 +52,17 @@ def exists(l, element):
         return True
     return exists(l[1:], element)
 
+
+def replace_symbol(string, old_symbol, new_symbol):
+    if not string:
+        return ""
+    if string[0] == old_symbol:
+        return new_symbol + replace_symbol(string[1:], old_symbol, new_symbol)
+    return string[0] + replace_symbol(string[1:], old_symbol, new_symbol)
+
+
 # returns list l with only unique values
-# currently O(n^2), will need to optimize using sorting
+# currently O(n^2)
 def unique(l):
     if not l:
         return []
@@ -54,22 +70,53 @@ def unique(l):
         return unique(l[1:])
     return [l[0]] + unique(l[1:])
 
-def unique_when_sorted_helper(sorted_list, curr_el):
+
+def unique_given_sorted_helper(sorted_list, curr_el):
     if not sorted_list:
         return []
     if sorted_list == curr_el:
-        return unique_when_sorted_helper(sorted_list[1:], curr_el)
-    return [sorted_list[0]] + unique_when_sorted_helper(sorted_list[1:], sorted_list[0])
+        return unique_given_sorted_helper(sorted_list[1:], curr_el)
+    return [sorted_list[0]] + unique_given_sorted_helper(sorted_list[1:], sorted_list[0])
 
 # returns list l with only unique values, assuming list l is sorted
 # optimized time complexity: O(n) instead of O(n^2)
-def unique_when_sorted(sorted_list):
+def unique_given_sorted(sorted_list):
     if not sorted_list:
         return []
-    return [sorted_list[0]] + unique_when_sorted_helper(sorted_list[1:], sorted_list[0])
+    return [sorted_list[0]] + unique_given_sorted_helper(sorted_list[1:], sorted_list[0])
+
 
 def text_input_to_sorted_word_set(string):
-    return unique_when_sorted(sort(list(map(to_lower, delimiter(remove_symbols(string, ["!", "?", "'", "\"", ".", ",", "/", "\\", "~", "-", "(", ")"]), " ")))))
+    # expanded for readability---lovely scheme-esque formatting :)
+
+    # removes punctuation, replaces \t and \n with spaces so that the delimiter can capture all words, then sets all words to lowercase
+    # afterwards, sorts the list of words, then removes elements for a unique list
+    return unique_given_sorted(
+        sort(
+            list(
+                map(
+                    to_lower,
+                    delimiter(
+                        replace_symbol(
+                            replace_symbol(
+                                remove_symbols(
+                                    string,
+                                    ["!", "?", "'", "\"", ".", ",",
+                                     "/", "\\", "~", "-", "(", ")"]
+                                ),
+                                "\n",
+                                " "
+                            ),
+                            "\t",
+                            " "
+                        ),
+                        " "
+                    )
+                )
+            )
+        )
+    )
+
 
 def merge(l1, l2):
     if not l1:
@@ -80,6 +127,7 @@ def merge(l1, l2):
         return [l1[0]] + merge(l1[1:], l2)
     return [l2[0]] + merge(l1, l2[1:])
 
+
 def merge_sort(l):
     if len(l) == 1:
         return l
@@ -88,6 +136,39 @@ def merge_sort(l):
 # sort list l ascending order using merge sort
 def sort(l):
     return merge_sort(l)
+
+# assumes sorted ordered set
+def difference_given_sorted_helper(set_1, set_2):
+    if not set_1:
+        return set_2
+    if not set_2:
+        return set_1
+    if set_1[0] == set_2[0]:
+        return difference_given_sorted_helper(set_1[1:], set_2[1:])
+    if set_1[0] < set_2[0]:
+        return [set_1[0]] + difference_given_sorted_helper(set_1[1:], set_2)
+    return [set_2[0]] + difference_given_sorted_helper(set_1, set_2[1:])
+
+def difference_given_sorted(set_1, set_2):
+    return sort(difference_given_sorted_helper(set_1, set_2))
+
+# assumes sorted ordered set
+def union_given_sorted(set_1, set_2):
+    return unique_given_sorted(sort(set_1 + set_2))
+
+# assumes sorted ordered set
+def intersection_given_sorted_helper(set_1, set_2):
+    if not set_1 or not set_2:
+        return []
+    if set_1[0] == set_2[0]:
+        return [set_1[0]] + intersection_given_sorted_helper(set_1[1:], set_2[1:])
+    if set_1[0] < set_2[0]:
+        return intersection_given_sorted_helper(set_1[1:], set_2)
+    return intersection_given_sorted_helper(set_1, set_2[1:])
+
+def intersection_given_sorted(set_1, set_2):
+    return sort(intersection_given_sorted_helper(set_1, set_2))
+
 def main():
 
     # file and sys I/O
@@ -104,10 +185,14 @@ def main():
     file2.close()
 
     # operations
-    word_set_1 = text_input_to_sorted_word_set(text1)
-    word_set_2 = text_input_to_sorted_word_set(text2)
-    print(word_set_1)
-    print(word_set_2)
+    sorted_word_set_1 = text_input_to_sorted_word_set(text1)
+    sorted_word_set_2 = text_input_to_sorted_word_set(text2)
+    print(sorted_word_set_1)
+    print(sorted_word_set_2)
+
+    print(difference_given_sorted(sorted_word_set_1, sorted_word_set_2))
+    print(union_given_sorted(sorted_word_set_1, sorted_word_set_2))
+    print(intersection_given_sorted(sorted_word_set_1, sorted_word_set_2))
 
 
 if __name__ == "__main__":
